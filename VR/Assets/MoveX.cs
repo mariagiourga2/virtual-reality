4f6eb6c83e33a8efd463af73bd0e3f7566e2e80c
mariagiourga2 <mariagiourga2000@gmail.com>
2023-09-30 19:05:00 +0300

    fixed character falling through terrain

--- VR/Assets/MoveX.cs
+++ VR/Assets/MoveX.cs
@@ -1,18 +1,25 @@
 using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class MoveX : MonoBehaviour
+
 {
+    float speed = 3f;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        transform.position=transform.position+ new Vector3(10f,0,0);
+        transform.position = transform.position + new Vector3(speed, 0, 0) * Time.deltaTime;
     }
+
 }
