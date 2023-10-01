8e25e7a11372f045b1ee6442d9b51a81aaa4cb9c
mariagiourga2 <mariagiourga2000@gmail.com>
2023-10-01 14:05:59 +0300

    port

--- VR/Assets/helloworld.cs
+++ VR/Assets/helloworld.cs
@@ -7,12 +7,12 @@ public class helloworld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello from Start!");
+        //Debug.Log("Hello from Start!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello World from Update!");
+        //Debug.Log("Hello World from Update!");
     }
 }
