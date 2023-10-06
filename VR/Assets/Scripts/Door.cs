ab6dc9059bba09709522e9c74ad9d21423dcc501
NIKOSTHEODOROPOULOS <nikos1036@hotmail.com>
2023-10-06 18:54:47 +0300

    menu

---
+++ VR/Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    /*public bool isOpen=false;
+    [SerializeField]
+    private bool isRotatingDoor = true;
+    [SerializeField]
+    private float Speed = 1f;
+    [SerializeField]
+    [Header("Rotation Configs")]
+    [SerializeField]
+    private float RotationAmount = 90f;
+    [SerializeField]
+    private float ForwardDirection = 0;
+   
+   // private Vec
+    */
+}
